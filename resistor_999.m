1ce5d1849f22baf89c2cd63f827be0f4d8de33e1
pswaszek <swaszek@uri.edu>
2025-12-06 17:12:01 -0500

    Add files via upload

---
+++ resistor_999.m
@@ -0,0 +1,4 @@
+function Req = resistor_999(list)
+%% computes the equivalent resistance for a given
+% resistor network
+Req= 35;
---
+++ spiral_999.m
@@ -0,0 +1,4 @@
+function [mat,rem]= spiral_999(list)
+%% constructs the spiral matrix and its remainder
+mat = [ 1 2 3; 8 9 4; 7 6 5 ];
+rem = [ 10 ];
